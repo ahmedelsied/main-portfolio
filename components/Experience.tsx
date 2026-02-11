f945b981450fbad1926d240affe26ac48bb37239
ahmed elsayed <43114893+ahmedelsied@users.noreply.github.com>
2026-02-11 21:11:07 +0200

    Update award link title syntax in Experience component

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -213,8 +213,8 @@ export default function Experience() {
                   </div>
                   <div className="flex-1">
                     <h4 className="text-xl font-bold text-gray-900 dark:text-white mb-2">
-                      <a href="https://developer.huawei.com/consumer/en/activity/digixActivity/digixWinnersDetail/201655277856046898" target="_blank" title="Details about the {{ award.title }} award">
-                        {{ award.title }}
+                      <a href="https://developer.huawei.com/consumer/en/activity/digixActivity/digixWinnersDetail/201655277856046898" target="_blank" title={`Details about the ${award.title} award`}>
+                        {award.title}
                       </a>
                     </h4>
                     <p className="text-primary-600 font-semibold mb-2">
